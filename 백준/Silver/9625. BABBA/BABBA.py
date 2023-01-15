d4768b03a5e63f68e9e492c5c1c0efa04e91f093
Hyun Soo Kim <93048764+KIMHYUNSOO1999@users.noreply.github.com>
2023-01-15 18:59:34 +0900

    [Silver V] Title: BABBA, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,12 @@
+K=int(input())
+
+a = [1, 0]
+b = [0, 1]
+ 
+for i in range(2, K+1):
+    a_num = a[i-1] + a[i-2]
+    a.append(a_num)
+    b_num = b[i-1] + b[i-2]
+    b.append(b_num)
+ 
+print(a[-1], b[-1])
