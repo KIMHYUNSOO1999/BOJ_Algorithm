c8628e17e6e80781caa8938fe74642a01eb05893
Hyun Soo Kim <93048764+KIMHYUNSOO1999@users.noreply.github.com>
2023-02-07 08:19:44 +0900

    [Silver IV] Title: 상근이의 여행, Time: 152 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+
+for _ in range(int(input())):
+    
+    N, M = map(int, input().rstrip().split())
+    
+    for _ in range(M):
+        u, v = map(int, input().rstrip().split())
+        
+    print(N-1)
