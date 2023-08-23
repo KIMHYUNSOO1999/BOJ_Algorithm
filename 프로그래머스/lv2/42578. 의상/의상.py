accbfc2f428ea5d86eab1b3bc41f7ccdcd4b6a16
Hyun Soo Kim <93048764+KIMHYUNSOO1999@users.noreply.github.com>
2023-08-23 13:08:23 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -0,0 +1,16 @@
+def solution(clothes):
+    
+    temp={}
+    
+    for i in clothes:
+        if i[1] not in temp:
+            temp[i[1]]=1
+        else:
+            temp[i[1]]+=1
+        
+    answer=1
+    
+    for i in temp.values():
+        answer*=(i+1)
+    
+    return answer-1
