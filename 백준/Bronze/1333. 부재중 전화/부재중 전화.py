98fcaebed1280a9c05e1d574d4ef1ec473b8dcec
H. S. Kim <saviorkhs@gmail.com>
2025-05-11 17:36:49 +0900

    [Bronze I] Title: 부재중 전화, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, L, D = map(int, input().split())
+
+L+=5
+num=0
+time=D
+
+for i in range(N):
+    num+=L
+    while True:
+        if time<num-5:
+            time+=D
+        else:
+            break
+    if num-5<=time<num:
+        break
+        
+print(time)
