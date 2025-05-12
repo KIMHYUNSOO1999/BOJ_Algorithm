59ab0e4e9f612da343d7530c3fe8dddf28eb06ff
H. S. Kim <saviorkhs@gmail.com>
2025-05-12 13:06:46 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+while True:
+    flag = True
+    for i in str(n):
+        if i != '4' and i != '7':
+            flag = False
+            n -= 1
+    if flag:
+        print(n)
+        break
