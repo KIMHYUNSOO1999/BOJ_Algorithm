532a7f059d688c9190fdaab68291e49eed73fb21
H. S. Kim <saviorkhs@gmail.com>
2025-07-07 10:23:06 +0900

    [Bronze I] Title: 색종이, Time: 180 ms, Memory: 21732 KB, Score: 42 point -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] visited = new int[1001][1001];
+        
+        for(int i=1;i<=n;i++){
+            
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int w = sc.nextInt();
+            int h = sc.nextInt();
+
+            for(int j = x ; j < x+w ; j++){
+                for(int k = y ; k < y+h ; k++)
+                    visited[j][k] = i;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 0; j <= 100; j++) {
+                for (int k = 0; k <= 100; k++) {
+                    if (visited[j][k] == i) count++;
+                }
+            }
+            System.out.println(count);
+        }
+        
+    }
+}
